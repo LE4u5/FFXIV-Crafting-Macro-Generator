e6ab143da3d1b0b3a4d54c844ed2280060f21bf2
Terrance Davenport <58961998+LE4u5@users.noreply.github.com>
2021-09-26 20:58:15 -0400

    init commit

---
+++ src/actions.js
@@ -0,0 +1,201 @@
+export let ACTIONS = [
+    {
+        "name" : "Basic Synthesis",
+        "wait" : "3",
+        "icon" : "basic_synthesis.png",
+        "cp" : 0
+    },
+    {
+        "name" : "Basic Touch",
+        "wait" : "3",
+        "icon" : "basic_touch.png",
+        "cp" : 18
+    },
+    {
+        "name" : "Master's Mend",
+        "wait" : "2",
+        "icon" : "master's_mend.png",
+        "cp" : 88
+    },
+    {
+        "name" : "Hasty Touch",
+        "wait" : "3",
+        "icon" : "hasty_touch.png",
+        "cp" : 30
+    },
+    {
+        "name" : "Rapid Synthesis",
+        "wait" : "3",
+        "icon" : "rapid_synthesis.png",
+        "cp" : 0
+    },
+    {
+        "name" : "Inner Quiet",
+        "wait" : "2",
+        "icon" : "inner_quiet.png",
+        "cp" : 18
+    },
+    {
+        "name" : "Observe",
+        "wait" : "2",
+        "icon" : "observe.png",
+        "cp" : 12
+    },
+    {
+        "name" : "Tricks of the Trade",
+        "wait" : "2",
+        "icon" : "tricks_of_the_trade.png",
+        "cp" : 0
+    },
+    {
+        "name" : "Waste Not",
+        "wait" : "2",
+        "icon" : "waste_not.png",
+        "cp" : 56
+    },
+    {
+        "name" : "Veneration",
+        "wait" : "2",
+        "icon" : "veneration.png",
+        "cp" : 18
+    },
+    {
+        "name" : "Standard Touch",
+        "wait" : "3",
+        "icon" : "standard_touch.png",
+        "cp" : 32
+    },
+    {
+        "name" : "Great Strides",
+        "wait" : "2",
+        "icon" : "great_strides.png",
+        "cp" : 32
+    },
+    {
+        "name" : "Innovation",
+        "wait" : "2",
+        "icon" : "innovation.png",
+        "cp" : 18
+    },
+    {
+        "name" : "Name of the Elements",
+        "wait" : "2",
+        "icon" : "name_of_the_elements.png",
+        "cp" : 30
+    },
+    {
+        "name" : "Brand of the Elements",
+        "wait" : "3",
+        "icon" : "brand_of_the_elements.png",
+        "cp" : 6
+    },
+    {
+        "name" : "Final Appraisal",
+        "wait" : "3",
+        "icon" : "final_appraisal.png",
+        "cp" : 1
+    },
+    {
+        "name" : "Waste Not II",
+        "wait" : "2",
+        "icon" : "waste_not_ii.png",
+        "cp" : 98
+    },
+    {
+        "name" : "Byregot's Blessing",
+        "wait" : "3",
+        "icon" : "byregot's_blessing.png",
+        "cp" : 24
+    },
+    {
+        "name" : "Precise Touch",
+        "wait" : "3",
+        "icon" : "precise_touch.png",
+        "cp" : 18
+    },
+    {
+        "name" : "Muscle Memory",
+        "wait" : "2",
+        "icon" : "muscle_memory.png",
+        "cp" : 6
+    },
+    {
+        "name" : "Careful Synthesis",
+        "wait" : "3",
+        "icon" : "careful_synthesis.png",
+        "cp" : 7
+    },
+    {
+        "name" : "Patient Touch",
+        "wait" : "3",
+        "icon" : "patient_touch.png",
+        "cp" : 6
+    },
+    {
+        "name" : "Manipulation",
+        "wait" : "2",
+        "icon" : "manipulation.png",
+        "cp" : 96
+    },
+    {
+        "name" : "Prudent Touch",
+        "wait" : "3",
+        "icon" : "prudent_touch.png",
+        "cp" : 25
+    },
+    {
+        "name" : "Focused Synthesis",
+        "wait" : "3",
+        "icon" : "focused_synthesis.png",
+        "cp" : 5
+    },
+    {
+        "name" : "Focused Touch",
+        "wait" : "3",
+        "icon" : "focused_touch.png",
+        "cp" : 18
+    },
+    {
+        "name" : "Reflect",
+        "wait" : "3",
+        "icon" : "reflect.png",
+        "cp" : 24
+    },
+    {
+        "name" : "Preparatory Touch",
+        "wait" : "3",
+        "icon" : "preparatory_touch.png",
+        "cp" : 40
+    },
+    {
+        "name" : "Groundwork",
+        "wait" : "3",
+        "icon" : "groundwork.png",
+        "cp" : 18
+    },
+    {
+        "name" : "Delicate Synthesis",
+        "wait" : "3",
+        "icon" : "delicate_synthesis.png",
+        "cp" : 32
+    },
+    {
+        "name" : "Intensive Synthesis",
+        "wait" : "3",
+        "icon" : "intensive_synthesis.png",
+        "cp" : 6
+    },
+    {
+        "name" : "Trained Eye",
+        "wait" : "3",
+        "icon" : "trained_eye.png",
+        "cp" : 250
+    },
+    {
+        "name" : "Careful Observation",
+        "wait" : "3",
+        "icon" : "careful_observation.png",
+        "cp" : 0
+    },
+]
+//module.exports = ACTIONS;
---
+++ src/main.js
@@ -0,0 +1,114 @@
+import {ACTIONS} from './actions.js';
+
+const stack = [];
+let div = document.querySelector('.output-cont');
+let textArea = document.createElement('textarea');
+let selected = document.querySelector('.selected-icons');
+let controls = document.querySelector('.controls-cont');
+
+function pushOnStack(action,stack){
+    stack.push(action);
+    setSelectedList();
+}
+
+function popOffStack(stack){
+    stack.pop();
+    setSelectedList();
+}
+
+function clearStack(stack){
+    length = stack.length
+    for(let i = 0; i < length; i++){
+        stack.pop();
+    }
+    textArea.innerHTML =``;
+    setSelectedList();
+}
+
+function generateMacro(actions){
+    let string =``;
+    let line = 0
+    let macroNum = 1;
+    for (let action of actions){
+        if(macroNum > 1 && line%15 === 0){string += '\n'}
+        if(line%15 === 0){string = string + `Macro #${macroNum}\n\n`;macroNum++}
+        string = string + `/ac "${action.name}" <wait.${action.wait}>\n`
+        line++;
+    }
+    return string;
+}
+
+function setSelectedList(){
+    selected.childNodes.forEach((n) =>{n.remove()});
+    let cont = document.createElement('div');
+    for(let action of stack){
+        let temp = document.createElement('img')
+        temp.setAttribute('src',`./assets/images/icons/${action.icon}`);
+        temp.setAttribute('alt',`${action.name}`);
+        temp.setAttribute('class',`action-icon`);
+        temp.addEventListener('click',()=>{
+            popOffStack(action,stack)
+        })
+        cont.appendChild(temp);
+    }
+    selected.appendChild(cont);
+}
+
+function totalCP(actions){
+    let cpTotal = 0;
+    for(let action of actions){
+        cpTotal += action.cp;
+    }
+    return cpTotal
+}
+
+function actionList(actions){
+    let div = document.createElement('div');
+    for(let action of actions){
+        let temp = document.createElement('img');
+        temp.setAttribute('src',`./assets/images/icons/${action.icon}`);
+        temp.setAttribute('class',`action-icon`);
+        temp.addEventListener('click',()=>{
+            pushOnStack(action,stack)
+        })
+        temp.addEventListener('mouseover',() => {
+            document.querySelector('.name-display').innerHTML = action.name;
+        });
+        temp.addEventListener('mouseout',() => {
+            document.querySelector('.name-display').innerHTML = '';
+        });
+        div.appendChild(temp);
+    }
+    document.querySelector('.action-cont').appendChild(div);
+}
+
+function attachControls(ctrls = document.createElement('div')){
+    let generate = document.createElement('input');
+    let _delete = document.createElement('input');
+    let clear = document.createElement('input');
+
+    generate.setAttribute('value', 'Generate')
+    generate.setAttribute('type', 'button')
+    generate.addEventListener('click',() => {
+        textArea.innerHTML =`${generateMacro(stack)}`;
+    })
+    _delete.setAttribute('value', 'Delete')
+    _delete.setAttribute('type', 'button')
+    _delete.addEventListener('click',() => {
+        popOffStack(stack);
+    })
+    clear.setAttribute('value', 'Clear')
+    clear.setAttribute('type', 'button')
+    clear.addEventListener('click',() => {
+        clearStack(stack)
+    })
+    ctrls.appendChild(generate);
+    ctrls.appendChild(_delete);
+    ctrls.appendChild(clear);
+}
+
+attachControls(controls);
+textArea.setAttribute('class','output');
+div.appendChild(textArea);
+actionList(ACTIONS);
+document.querySelector('.tt').appendChild(div);
